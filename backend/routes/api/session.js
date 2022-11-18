02ada99f44767348d67845d12be0799b98279f43
Ciara <ciarafumar@gmail.com>
2022-11-18 09:17:04 -0800

    Add User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,33 +6,53 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+// backend/routes/api/session.js
+// ...
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+// ...
+
 
 // backend/routes/api/session.js
 // ...
 
+const validateLogin = [
+    check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+    check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
+
+// backend/routes/api/session.js
+// ...
 // Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
-        const { credential, password } = req.body;
+      const { credential, password } = req.body;
 
-        const user = await User.login({ credential, password });
+      const user = await User.login({ credential, password });
 
-        if (!user) {
-            const err = new Error('Login failed');
-            err.status = 401;
-            err.title = 'Login failed';
-            err.errors = ['The provided credentials were invalid.'];
-            return next(err);
-        }
+      if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = ['The provided credentials were invalid.'];
+        return next(err);
+      }
 
-        await setTokenCookie(res, user);
+      await setTokenCookie(res, user);
 
-        return res.json({
-            user
-        });
+      return res.json({
+        user
+      });
     }
-);
+  );
 
     // backend/routes/api/session.js
 // ...
@@ -66,6 +86,7 @@ router.get(
   );
 
   // ...
+
 
 
     module.exports = router;
