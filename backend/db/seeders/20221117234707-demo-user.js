b04b54f0b9ae0e3affaaa0ebccaf03d19446bd75
Ciara <ciarafumar@gmail.com>
2022-11-21 11:16:58 -0800

    seed file update

--- backend/db/seeders/20221117234707-demo-user.js
+++ backend/db/seeders/20221117234707-demo-user.js
@@ -11,16 +11,22 @@ module.exports = {
     options.tableName = 'Users';
     return queryInterface.bulkInsert(options, [
       {
+        firstName: 'demo',
+        lastName: 'demoo',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'julian',
+        lastName: 'neal',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'jeremiah',
+        lastName: 'john',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
