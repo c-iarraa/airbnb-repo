e279570f363f7a1f3c782f77e4cfe68e9562828e
Ciara <ciarafumar@gmail.com>
2022-11-23 16:25:40 -0800

    options object

--- backend/db/migrations/20221122233915-create-booking.js
+++ backend/db/migrations/20221122233915-create-booking.js
@@ -1,11 +1,11 @@
 'use strict';
+/** @type {import('sequelize-cli').Migration} */
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Bookings', {
@@ -45,7 +45,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   down: async (queryInterface, Sequelize) => {
     options.tableName = 'Bookings'
