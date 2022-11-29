2b35c0d78786184472725e462dd285aa4cfdacec
Ciara <ciarafumar@gmail.com>
2022-11-29 01:36:19 -0800

    debugging keys

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -10,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Review.belongsTo(models.User, { foreignLey: 'userId', onDelete: 'CASCADE', hooks: true });
-      Review.belongsTo(models.Spot, { foreignKey: 'spotId', onDelete: 'CASCADE', hooks: true });
+      Review.belongsTo(models.User, { foreignKey: 'userId'});
+      Review.belongsTo(models.Spot, { foreignKey: 'spotId'});
       Review.hasMany(models.ReviewImage, { foreignKey: 'reviewId', onDelete: 'CASCADE', hooks: true });
     }
   }
--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Spot.belongsTo(models.User, { foreignKey: 'ownerId', onDelete: 'CASCADE', hooks: true });
+      Spot.belongsTo(models.User, { foreignKey: 'ownerId'});
       Spot.hasMany(models.SpotImage, { foreignKey: 'spotId', onDelete: 'CASCADE', hooks: true});
       Spot.hasMany(models.Booking, { foreignKey: 'spotId', onDelete: 'CASCADE', hooks: true });
       Spot.hasMany(models.Review, { foreignKey: 'spotId', onDelete: 'CASCADE', hooks: true });
