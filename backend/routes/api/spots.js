0877195aed2fda13a347722e1b9e1fdae3e20337
Ciara <ciarafumar@gmail.com>
2022-11-28 23:30:00 -0800

    spots api file

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // backend/routes/api/users.js
 // ...
 const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
+const { handleValidationerrorsObj } = require('../../utils/validation');
 const { notifyAboutExistingFile } = require('sequelize-cli/lib/helpers/view-helper');
 const { json } = require('sequelize');
 // ...
@@ -116,7 +116,7 @@ router.post('/:spotId/bookings', requireAuth, async (req, res) => {
         return res.status(400).json({
             "message": "Validation error",
             "statusCode": 400,
-            "errors": [
+            "errorsObj": [
                 "endDate cannot be on or before startDate"
             ]
         });
@@ -135,7 +135,7 @@ router.post('/:spotId/bookings', requireAuth, async (req, res) => {
             res.status(403).json({
                 "message": "Sorry, this spot is already booked for the specified dates",
                 "statusCode": 403,
-                "errors": {
+                "errorsObj": {
                     "startDate": "Start date conflicts with an existing booking",
                     "endDate": "End date conflicts with an existing booking"
                 }
@@ -147,7 +147,7 @@ router.post('/:spotId/bookings', requireAuth, async (req, res) => {
             res.status(403).json({
                 "message": "Sorry, this spot is already booked for the specified dates",
                 "statusCode": 403,
-                "errors": {
+                "errorsObj": {
                     "startDate": "Start date conflicts with an existing booking",
                     "endDate": "End date conflicts with an existing booking"
                 }
@@ -159,7 +159,7 @@ router.post('/:spotId/bookings', requireAuth, async (req, res) => {
             res.status(403).json({
                 "message": "Sorry, this spot is already booked for the specified dates",
                 "statusCode": 403,
-                "errors": {
+                "errorsObj": {
                     "startDate": "Start date conflicts with an existing booking",
                     "endDate": "End date conflicts with an existing booking"
                 }
@@ -269,7 +269,7 @@ router.post('/:spotId/reviews', requireAuth, async(req,res) => {
         res.status(400).json({
             "message": "Validation error",
             "statusCode": 400,
-            errors: {
+            errorsObj: {
                 "review": "Review text is required",
                 "stars": "Stars must be an integer from 1 to 5"
             }
@@ -331,7 +331,7 @@ router.put('/:spotId', requireAuth, async (req, res)=> {
             res.status(400).json({
                 message: "Validation Error",
                 statusCode: 400,
-                errors: {
+                errorsObj: {
                 address: "Street address is required",
                 city: "City is required",
                 state: "State is required",
@@ -372,7 +372,7 @@ router.post('/', requireAuth, async(req, res)=> {
         res.status(400).json({
           message: "Validation Error",
           statusCode: 400,
-          errors: {
+          errorsObj: {
             address: "Street address is required",
             city: "City is required",
             state: "State is required",
@@ -515,95 +515,292 @@ router.get("/:spotId", requireAuth, async(req, res) => {
 // --------------------------------------------------------------------------------------------------
 
 // GET ALL SPOTS
+router.get("/",  async (req, res, next) => {
+    let { page, size, minLat, maxLat, minLng, maxLng, minPrice, maxPrice } = req.query
 
-router.get("/", async(req, res) => {
+    let errorsObj = {}
+    let errorMarker = false
 
-  let { page, size } = req.query;
-  if(page < 1 || size < 1){
-    res.status(400).json({
-      "message": "Validation Error",
-      "statusCode": 400,
-      "errors": {
-        "page": "Page must be greater than or equal to 1",
-        "size": "Size must be greater than or equal to 1",
-        "maxLat": "Maximum latitude is invalid",
-        "minLat": "Minimum latitude is invalid",
-        "minLng": "Maximum longitude is invalid",
-        "maxLng": "Minimum longitude is invalid",
-        "minPrice": "Maximum price must be greater than or equal to 0",
-        "maxPrice": "Minimum price must be greater than or equal to 0"
-      }
-    });
-  };
+    if (page && page < 1){
+        errorsObj.page = "Page must be greater than or equal to 1"
+        errorMarker = true
+    }
 
-  let pagination = {}
-  if (parseInt(page) >= 1 && parseInt(size) >= 1) {
-      pagination.limit = size
-      pagination.offset = size * (page - 1)
-  }
+    if (page && !Number.isInteger(parseInt(page))){
+        errorsObj.page = "Page must be greater than or equal to 1"
+        errorMarker = true
+    }
 
+    if (size && size < 1){
+        errorsObj.size = "Size must be greater than or equal to 1"
+        errorMarker = true
+    }
 
-    const spots = await Spot.findAll({
-        include: [
-            {
-                model: SpotImage
-            },
-            {
-                model: Review
-            }
-        ]
-    });
+    if (size && !Number.isInteger(parseInt(size))){
+        errorsObj.size = "Size must be greater than or equal to 1"
+        errorMarker = true
+    }
+
+    if (maxLat && maxLat.indexOf('.') === -1){
+        errorsObj.maxLat = "Maximum latitude is invalid"
+        errorMarker = true
+    }
+
+    if (maxLat && !Number.isInteger(parseInt(maxLat))){
+        errorsObj.minLat = "Maximum latitude is invalid"
+        errorMarker = true
+    }
+
+    if (minLat && !Number.isInteger(parseInt(minLat))){
+        errorsObj.minLat = "Minimum latitude is invalid"
+        errorMarker = true
+    }
+
+    if (minLat && minLat.indexOf('.') === -1){
+        errorsObj.minLat = "Minimum latitude is invalid"
+        errorMarker = true
+    }
+
+    if (minLng && minLng.indexOf('.') === -1){
+        errorsObj.minLng = "Minimum longitude is invalid"
+        errorMarker = true
+    }
+
+    if (minLng && !Number.isInteger(parseInt(minLng))){
+        errorsObj.minLng = "Minimum longitude is invalid"
+        errorMarker = true
+    }
+
+    if (maxLng && maxLng.indexOf('.') === -1){
+        errorsObj.maxLng = "Maximum longitude is invalid"
+        errorMarker = true
+    }
+
+    if (maxLng && !Number.isInteger(parseInt(maxLng))){
+        errorsObj.maxLng = "Maximum longitude is invalid"
+        errorMarker = true
+    }
+
+    if (minPrice && parseInt(minPrice) <= 0){
+        errorsObj.minPrice = "Minimum price must be greater than or equal to 0"
+        errorMarker = true
+    }
+
+    if (minPrice && !Number.isInteger(parseInt(minPrice))){
+        errorsObj.minPrice = "Minimum price must be greater than or equal to 0"
+        errorMarker = true
+    }
+
+    if (maxPrice && parseInt(maxPrice) <= 0){
+        errorsObj.maxPrice = "Maximum price must be greater than or equal to 0"
+        errorMarker = true
+    }
+
+    if (maxPrice && !Number.isInteger(parseInt(maxPrice))){
+        errorsObj.maxPrice = "Maximum price must be greater than or equal to 0"
+        errorMarker = true
+    }
+
+    if (errorMarker === true){
+        return res.status(400).json({
+            message: "Validation Error",
+            "statusCode": 400,
+            errorsObj
+          });
+    };
+
+    if (!page){
+        page = 1
+    };
+
+    if (page >= 10){
+        page = 10
+    };
+
+    if (!size || size >= 20){
+        size = 20
+    };
+
+    let paginationObj = {};
+
+    page = parseInt(page)
+    size = parseInt(size)
+
+    if (page >= 1 && size >= 1){
+        paginationObj.limit = size,
+        paginationObj.offset = size*(page-1)
+    }
+
+    let where = {}
+
+    if (maxLat && minLat){
+        where.lat = {
+            [Op.between]: [minLat, maxLat]
+        }
+    }
+
+    if (maxLat && !minLat){
+        where.lat = {
+            [Op.lte]: maxLat
+        }
+    }
+
+    if (minLat && !maxLat){
+        where.lat = {
+            [Op.gte]: minLat
+        }
+    }
+
+    if (maxLng && minLng){
+        where.lng = {
+            [Op.between]: [minLng, maxLng]
+        }
+    }
+
+    if (maxLng && !minLng){
+        where.lng = {
+            [Op.lte]: maxLng
+        }
+    }
+
+    if (minLng && ! maxLat){
+        where.lng = {
+            [Op.gte]: minLng
+        }
+    }
+
+    if (maxPrice && minPrice){
+        where.price = {
+            [Op.between]: [minPrice, maxPrice]
+        }
+    }
+
+    if (maxPrice && !minPrice){
+        where.price = {
+            [Op.lte]: maxPrice
+        }
+    }
+
+    if (minPrice && !maxPrice){
+        where.price = {
+            [Op.gte]: minPrice
+        }
+    }
+
+    console.log(where)
+  let spots = await Spot.findAll({
+    include: [
+        {
+        model: Review
+        },
+        {
+        model: SpotImage
+    }
+],
+    ...paginationObj
+  });
 
     // spots.toJSON()
     // console.log(spots)
-    let spotsList = [];
-    spots.forEach(spot => {
-        spotsList.push(spot.toJSON()); // basically converts to javascript so we can edit/make changes to code
-    });
+  let spotsList = []
+  spots.forEach(spot => {
+    spotsList.push(spot.toJSON())
+  })
 
-    spotsList.forEach(spot => {
-        const stars = [];
-        if(!spot.Reviews.length) {
-            spots.ratings = "No ratings at the moment"
-            // delete spots.Reviews
+  spotsList.forEach(spot => {
+      let totalStars = 0
+    spot.Reviews.forEach(review => {
+        totalStars += review.stars
+    })
+    spot.avgRating = totalStars/spot.Reviews.length
+    delete spot.Reviews
+  })
+
+  spotsList.forEach(spot => {
+    spot.SpotImages.forEach(image => {
+        if (image.preview === true){
+            spot.previewImage = image.url
         }
+    })
+    if (!spot.previewImage){
+        spot.previewImage = 'No preview image found'
+    }
+    delete spot.SpotImages
+  })
 
-        spot.Reviews.forEach(el => {
-            // console.log(el.stars)
-            // const avgRating = [];
-            if (el.stars) {
-                stars.push(el.stars)
-            } else {
-                spots.avgRating = 'No Ratings Yet'
-            }
-            // console.log(stars)
-            let total = stars.reduce((previous, current) => previous + current );
-            // console.log(total/stars.length)
-            spot.avgRating = total/stars.length
-            if(!spots.avgRating){
-                spots.avgRating = 'There are no ratings for this location at this time'
-              }
-        })
-        delete spot.Reviews
-    });
-
-    // preview image code
-    spotsList.forEach(spot => {
-        spot.SpotImages.forEach(image => {
-            // console.log(image.preview)
-            if (image.preview === true) {
-                // console.log(image)
-                spot.previewImage = image.url
-            };
-        });
-
-        if (!spot.previewImage) {
-            spot.previewImage = "no preview image found"
-        }
-        delete spot.SpotImages
-    });
-    res.json(spotsList);
+  return res.json({
+    Spots: spotsList,
+    page,
+    size
+  });
 });
+
+
+// router.get("/", async(req, res) => {
+
+
+
+//     const spots = await Spot.findAll({
+//         include: [
+//             {
+//                 model: SpotImage
+//             },
+//             {
+//                 model: Review
+//             }
+//         ]
+//     });
+
+//     // spots.toJSON()
+//     // console.log(spots)
+//     let spotsList = [];
+//     spots.forEach(spot => {
+//         spotsList.push(spot.toJSON()); // basically converts to javascript so we can edit/make changes to code
+//     });
+
+//     spotsList.forEach(spot => {
+//         const stars = [];
+        // if(!spot.Reviews.length) {
+        //     spots.ratings = "No ratings at the moment"
+        //     // delete spots.Reviews
+        // }
+
+        // spot.Reviews.forEach(el => {
+        //     // console.log(el.stars)
+        //     // const avgRating = [];
+        //     if (el.stars) {
+        //         stars.push(el.stars)
+        //     } else {
+        //         spots.avgRating = 'No Ratings Yet'
+        //     }
+        //     // console.log(stars)
+        //     let total = stars.reduce((previous, current) => previous + current );
+        //     // console.log(total/stars.length)
+        //     spot.avgRating = total/stars.length
+        //     if(!spots.avgRating){
+        //         spots.avgRating = 'There are no ratings for this location at this time'
+        //       }
+    //     })
+    //     delete spot.Reviews
+    // });
+
+    // // preview image code
+    // spotsList.forEach(spot => {
+    //     spot.SpotImages.forEach(image => {
+    //         // console.log(image.preview)
+    //         if (image.preview === true) {
+    //             // console.log(image)
+    //             spot.previewImage = image.url
+    //         };
+    //     });
+
+    //     if (!spot.previewImage) {
+    //         spot.previewImage = "no preview image found"
+    //     }
+    //     delete spot.SpotImages
+    // });
+    // res.json(spotsList);
+// });
 
 
 
